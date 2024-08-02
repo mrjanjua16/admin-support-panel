12c97f4433cda427c73bc5f57cc8394cbd205e3f
mrjanjua16 <imubashirahmad@gmail.com>
2024-08-02 16:09:34 +0500

    initial commit for client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-4xl font-bold text-center'>
+        Admin Support Portal
+      </div>
+    </>
+  )
+}
+
+export default App
